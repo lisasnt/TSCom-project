0410e2fcfe238151edf8172071e78834fa33a993
Espenvb <espenvb12@gmail.com>
2024-11-30 10:12:47 +0000

    Sensor Fusion Progress

--- ArduinoTSCom/SensorFusion.h
+++ ArduinoTSCom/SensorFusion.h
@@ -3,4 +3,4 @@ float tiltComp(distance, angle);
 //Should find out what color is most likely to be reflecting. 
 int colorDetect(Ultra_sound_dsitance, infrared_measurment);
 float colorComp(infrared_distance);
-float predDistance(Ultra_sound_distance, infrared_distance);
+float predDistance(d_ultra, d_infra, w_utlra, w_infra);
--- ArduinoTSCom/SensorFusion.ino
+++ ArduinoTSCom/SensorFusion.ino
@@ -12,16 +12,28 @@ int colorDetect(Ultra_sound_dsitance, infrared_measurment){
     */
 }
 
-float colorComp(infrared_measurment){
+float colorComp(v_infra, d_ultra, d_ultra_treshold, v_infra_treshold, &color_comp){
     /*
         Need curves for different color(Teacher reccomended like 3)
         Use the curves to predict distance based on recived voltage
     */
+
+    if(d_ultra < d_ultra_treshold){
+        Serial.print("Warning: Infrared is not calibrated correclty due to distance to object being to short.")
+    }else if(v_infra < v_infra_treshold){
+        Serial.print("Warning: Infrared is not calibrated correclty due recvied signal is to weak.")
+    }else{
+        return color_comp
+    }
+
 }
 
-float predDistance(Ultra_sound_distance, infrared_distance){
+float predDistance(d_ultra, d_infra, w_utlra, w_infra){
     /*
         Create a system for weighing the differnet measurments
         Idea(Not necceceraly optimal): Find probability of each measurment being the right one, and find expected value based on this
     */
+
+    return w_ultra*d_ultra-w_infra*d_infra/(w_ultra + w_infra);
+
 }
